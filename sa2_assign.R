1cf18ab7647329e1c8de45508136407d1c50c8ad
VINEET KAPOOR <32532714+vin725k@users.noreply.github.com>
2018-09-18 00:22:53 +0530

    Add files via upload

--- sa2_assign.R
+++ sa2_assign.R
@@ -1,15 +1,16 @@
 library(readxl)
+library(MASS)
 
 df1<- read_excel("Dataset.xls")
 df2<- df1
 ##sample
-set.seed(123)
+set.seed(312)
 data=df1[sample(nrow(df1), 1000),]
 write.csv(data,"sampleset.csv")
 head(data)
 class(data)
 colnames(data)
-
+data$`AREA(Agrl)`
 str(data)
 sapply(data, function(x) sum(is.na(x)))
 
@@ -32,7 +33,6 @@ sapply(df, function(x) sum(is.na(x)))
 head(df)
 
 str(df)
-
 ##changing datatypes
 df$Gender <- as.factor(df$Gender)
 df$AGE <- as.numeric(df$AGE)
@@ -41,56 +41,140 @@ df$RELIGN <- as.factor(df$RELIGN)
 df$MTONGUE <- as.factor(df$MTONGUE)
 df$OCCU <- as.factor(df$OCCU)
 
+
+###VISUALIZING
 library(ggplot2)
-qplot(TOTAL, data=df, geom="bar", fill=Gender, alpha=I(.5)) ##right skewed
+g <- ggplot(df, aes(Gender,TOTAL))
+g + geom_bar(stat = "identity",fill = "blue") +
+  coord_flip() 
+g <- ggplot(df, aes(CASTE,TOTAL))
+g + geom_bar(stat = "identity",fill = "blue") +
+  coord_flip() 
+g <- ggplot(df, aes(RELIGN,TOTAL))
+g + geom_bar(stat = "identity",fill = "blue") +
+  coord_flip() 
+g <- ggplot(df, aes(MTONGUE,TOTAL))
+g + geom_bar(stat = "identity",fill = "blue") +
+  coord_flip() 
+g <- ggplot(df, aes(OCCU,TOTAL))
+g + geom_bar(stat = "identity",fill = "blue") +
+  coord_flip() 
+
+
 qplot(WRITE, data=df, geom="density", fill=Gender, alpha=I(.5)) ##normal
 qplot(READ, data=df, geom="density", fill=Gender, alpha=I(.5))  ##left skew
 qplot(MATH, data=df, geom="density", fill=Gender, alpha=I(.5))  ##left skew for female
 qplot(TOTAL, data=df, geom="density", fill=Gender, alpha=I(.5))  ##left skew
 qplot(TOTAL, data=df, geom="density", alpha=I(.5))  ##left skew
 
-M<-cor(df[,8:13])
-M
-head(round(M,2))
-corrplot::corrplot(M,method = "color")
+###boxplot
+boxplot(TOTAL~Gender,data=df)
+df$TOTAL[df$TOTAL< 20] <- 20
+boxplot(~Gender,data=df)
+boxplot(INCOME~Gender,data=df)
+boxplot(Area~Gender,data=df)
+boxplot(AGE~Gender,data=df)
+
+###outlier treatment
+ df$AGE[df$AGE > 50]  <- NA
+df <- df %>%na.omit()
+quantile(df$INCOME,0.95)
+
+df$INCOME[df$INCOME > 11000] <- 11000
+boxplot(INCOME~Gender,data=df)
+
+#####correlation matrix
+
+M<-df[,8:13]
+M$AGE<- df$AGE
+
+c <- cor(M)
+head(round(c,2))
+corrplot::corrplot(c,method = "color")
 library(car)
-vif(Model1)
+
+
+
+##creating dummy variables
+install.packages("fastDummies")
+require(fastDummies)
+df <- fastDummies::dummy_cols(df, select_columns = "Gender")
+df <- fastDummies::dummy_cols(df, select_columns = "OCCU")
+df <- fastDummies::dummy_cols(df, select_columns = "RELIGN")
+df <- fastDummies::dummy_cols(df, select_columns = "CASTE")
+df <- fastDummies::dummy_cols(df, select_columns = "MTONGUE")
+
+##total_Columns
+ncol(df)
+
+colnames(df)
+###modelling
+vif()
 df$INCOME <- (log(df$INCOME + 1))
 df <- df[-c(946, 572, 409,87), ]
 df$log_total <- log(df$TOTAL+1) 
-Model1=lm(df$TOTAL~log_Age + MATH,data=df)
 
+###
+require(leaps)
+reg <- regsubsets(TOTAL~AGE+INCOME+Gender_F+Gender_T+OCCU_C+OCCU_A+OCCU_H+OCCU_B+OCCU_U+RELIGN_H+RELIGN_C+RELIGN_M + CASTE_SC +CASTE_OT+CASTE_ST+MTONGUE_T+MTONGUE_K+MTONGUE_U+MTONGUE_D,data = df, nbest=10)
+summary(reg)
+m2 <- lm(TOTAL ~ AGE+ INCOME+Gender_F + CASTE_ST+ OCCU_B,data = df)
+summary(m2)
+
+###backward using regsubsets
+reg2 = regsubsets(TOTAL~AGE+INCOME+Gender_F+Gender_T+OCCU_C+OCCU_A+OCCU_H+OCCU_B+OCCU_U+RELIGN_H+RELIGN_C+RELIGN_M + CASTE_SC +CASTE_OT+CASTE_ST+MTONGUE_T+MTONGUE_K+MTONGUE_U+MTONGUE_D,data = df,nbest=10,method = "backward")
+summary(reg2)
+plot(reg2,scale="r2")
+
+
+m3 <- lm(TOTAL ~ AGE+ INCOME+Gender_F + CASTE_SC+ OCCU_H + OCCU_B + CASTE_OT + MTONGUE_T + MTONGUE_U ,data = df)
+summary(m3)
+
+
+####randomly
+
+m3 <- lm(TOTAL ~ AGE:Gender_F:CASTE_SC + CASTE_SC  +RELIGN_H:OCCU_B:AGE+MTONGUE_U+ CASTE_OT+ Gender_F+ CASTE_OT:Gender_F+ OCCU_U:MTONGUE_U  + OCCU_C:MTONGUE_U:CASTE_SC ,data = df)
+summary(m3)
+plot(m3)
+# Other useful functions 
+coefficients(m3) # model coefficients
+confint(m3, level=0.95) # CIs for model parameters 
+fitted(m3) # predicted values
+residuals(m3) # residuals
+anova(m3) # anova table 
+vcov(m3) # covariance matrix for model parameters 
+influence(m3) # regression diagnostics
+
+df <- df[-c(748,909,271),]
+df2<- df
+df <- df[-c(704,101,474),]
+df<- df2
+###CREATING new variables
+
+
+###backward elimination using stepaic
+Model1=lm(TOTAL~AGE+INCOME+Gender_F+Gender_T+OCCU_C+OCCU_A+OCCU_H+OCCU_B+OCCU_U+RELIGN_H+RELIGN_C+RELIGN_M + CASTE_SC +CASTE_OT+CASTE_ST+MTONGUE_T+MTONGUE_K+MTONGUE_U+MTONGUE_D,data=df)
+step <- stepAIC(Model1, direction="back")
+
+m <- lm(TOTAL~AGE+ CASTE_SC + CASTE_OT+ OCCU_B,data = df)
+summary(m)
 summary(Model1)
+
 plot(Model1)
-df[887,]
-
-residualPlot(Model1)
-qqPlot(Model1)
-
-df$READ[df$READ < 5] <- 5
-
-df$MATH[df$MATH == 0] <- mean(df$MATH)
-##standardizing
-x <- mean(data$TOTAL) - 1.5*sd(data$TOTAL)
-df$TOTAL[df$TOTAL < x] <- x
-y <- mean(data$TOTAL) + 1.5*sd(data$TOTAL)
-df$TOTAL[df$TOTAL > y] <- y
-
-
-##creating dummy
-install.packages("fastDummies")
-require(fastDummies)
-d <- fastDummies::dummy_cols(df, select_columns = "Gender")
-
-
-qplot(TOTAL, data=df, geom="density", fill=Gender, alpha=I(.5)) ##right skewed
 
 
 
-# Boxplot of MPG by Car Cylinders 
 
-df$log_Age <- log(df$CASTE)
 
-df$log_Age[df$log_Age > 4.0] <- 4.0
-boxplot(TOTAL~df$INCOME,data=df)
-df$TOTAL[df$TOTAL< 20] <- 20 
+###forward elimination
+
+
+
+
+
+###stepwise regression
+
+fit_Step <- lm(TOTAL~AGE+INCOME+Gender_F+Gender_T+OCCU_C+OCCU_A+OCCU_H+OCCU_B+OCCU_U+RELIGN_H+RELIGN_C+RELIGN_M + CASTE_SC +CASTE_OT+CASTE_ST+MTONGUE_T+MTONGUE_K+MTONGUE_U+MTONGUE_D,data=df)
+step <- stepAIC(fit_Step, direction="both")
+
+step$anova # display results
